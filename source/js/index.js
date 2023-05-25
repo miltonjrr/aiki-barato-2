4feaaff14f4060ad73dc4de973da0fc4466072d7
miltonjrr <miltonnasarojr@gmail.com>
2023-05-24 23:01:03 -0300

    adding login icon button, change font, login hide and show logic

--- source/js/index.js
+++ source/js/index.js
@@ -1,1 +1,11 @@
+let showLogin = document.getElementById("show-login");
+let closeLogin = document.getElementById("close-icon");
+let hiddenLogin = document.getElementById("login-box");
 
+showLogin.addEventListener("click", function () {
+  hiddenLogin.style.display = "block";
+});
+
+closeLogin.addEventListener("click", function () {
+  hiddenLogin.style.display = "none";
+});
